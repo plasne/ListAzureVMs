95cd723fcd879045207d994d3c349d0b54bf0699
plasne <plasne@iname.com>
2016-07-22 16:14:13 -0400

    init

---
+++ ListAzureVMs/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ListAzureVMs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnQuery = new System.Windows.Forms.Button();
+            this.listResults = new System.Windows.Forms.ListBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLogin.Location = new System.Drawing.Point(996, 12);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(151, 73);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnQuery
+            // 
+            this.btnQuery.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnQuery.Location = new System.Drawing.Point(996, 100);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(151, 70);
+            this.btnQuery.TabIndex = 1;
+            this.btnQuery.Text = "query";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
+            // 
+            // listResults
+            // 
+            this.listResults.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listResults.FormattingEnabled = true;
+            this.listResults.ItemHeight = 25;
+            this.listResults.Location = new System.Drawing.Point(12, 12);
+            this.listResults.Name = "listResults";
+            this.listResults.Size = new System.Drawing.Size(978, 704);
+            this.listResults.TabIndex = 2;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 718);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1159, 38);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(223, 33);
+            this.toolStripStatusLabel1.Text = "Click login to start...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1159, 756);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listResults);
+            this.Controls.Add(this.btnQuery);
+            this.Controls.Add(this.btnLogin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnQuery;
+        private System.Windows.Forms.ListBox listResults;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ ListAzureVMs/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System.Net;
+using System.Xml;
+
+namespace ListAzureVMs
+{
+    public partial class Form1 : Form
+    {
+
+        private AuthenticationResult auth;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                AuthenticationContext context = new AuthenticationContext("https://login.windows.net/peterlasne.onmicrosoft.com");
+                UserPasswordCredential credential = new UserPasswordCredential("service@peterlasne.onmicrosoft.com", "???????");
+                auth = await context.AcquireTokenAsync("https://management.core.windows.net/", "1beaa8ef-a7aa-40b9-958f-5b61e6feef36", credential);
+                toolStripStatusLabel1.Text = "Successfully logged in.";
+            }
+            catch (Exception ex)
+            {
+                toolStripStatusLabel1.Text = "Failed login: " + ex.Message;
+            }
+        }
+
+        private void btnQuery_Click(object sender, EventArgs e)
+        {
+            listResults.Items.Clear();
+
+            WebClient client = new WebClient();
+            client.Headers.Add("Authorization", "bearer " + auth.AccessToken);
+            client.Headers.Add("x-ms-version", "2015-04-01");
+            string services = client.DownloadString("https://management.core.windows.net/fb0d9e18-712a-426f-ae67-1eac274bf9f1/services/hostedservices");
+
+            XmlDocument servicesXml = new XmlDocument();
+            XmlNamespaceManager servicesNsm = new XmlNamespaceManager(servicesXml.NameTable);
+            servicesNsm.AddNamespace("a", "http://schemas.microsoft.com/windowsazure");
+            servicesXml.LoadXml(services);
+
+            foreach (XmlNode service in servicesXml.DocumentElement.SelectNodes("a:HostedService", servicesNsm))
+            {
+
+                // query to get the cloud service properties
+                string serviceName = service.SelectSingleNode("a:ServiceName", servicesNsm).InnerText;
+                string properties = client.DownloadString("https://management.core.windows.net/fb0d9e18-712a-426f-ae67-1eac274bf9f1/services/hostedservices/" + serviceName + "?embed-detail=true");
+                XmlDocument propertiesXml = new XmlDocument();
+                XmlNamespaceManager propertiesNsm = new XmlNamespaceManager(propertiesXml.NameTable);
+                propertiesNsm.AddNamespace("a", "http://schemas.microsoft.com/windowsazure");
+                propertiesXml.LoadXml(properties);
+
+                // get the instances
+                foreach (XmlNode deployment in propertiesXml.DocumentElement.SelectNodes("a:Deployments/a:Deployment/a:RoleInstanceList/a:RoleInstance", propertiesNsm))
+                {
+                    listResults.Items.Add(deployment.SelectSingleNode("a:InstanceName", propertiesNsm).InnerText);
+                }
+
+            }
+
+            toolStripStatusLabel1.Text = listResults.Items.Count + " classic VMs found.";
+
+        }
+    }
+}
